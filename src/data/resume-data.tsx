d6197da5ff3453726f7d644b13c245a63dc54cf2
BlameMe9 <110724383+BlameMe9@users.noreply.github.com>
2025-02-16 21:35:41 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,4 +1,3 @@
-
 import {
   AmbitLogo,
   BarepapersLogo,
@@ -27,9 +26,14 @@ export const RESUME_DATA = {
   locationLink: "https://www.google.com/maps/place/Oslo",
   about:
     "PhD stipend at NTNU and Work and Organizational consultant at Østlyng & Bjerke.",
-  summary:
-    "My career, educations and general interests have equipped me with comprehensive skills spanning, technology, psychology, research and organizational development. My main passion lies at the intersection of psychology and technology, especially in the application of AI and technologies to solve complex challenges for real world applications. My expertise shines in environments where agility and fast-pace are important. As a leader, I emphasizes transparency, sincerity, cooperation, and continues problem-solving.",
-  avatarUrl: "https://media.licdn.com/dms/image/v2/D4D03AQGnEDQokH4eHA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712494802583?e=1744848000&v=beta&t=HYiJoO4OI39uYECXIJlR9oPrTh77NOc7okkWwSgAE_8",
+  summary: [
+    "My career, educations and general interests have equipped me with comprehensive skills spanning technology, psychology, research and organizational development.",
+    "My main passion lies at the intersection of psychology and technology, especially in the application of AI and technologies to solve complex challenges for real world applications.",
+    "My expertise shines in environments where agility and fast-pace are important.",
+    "As a leader, I emphasize transparency, sincerity, cooperation, and continuous problem-solving.",
+  ],
+  avatarUrl:
+    "https://media.licdn.com/dms/image/v2/D4D03AQGnEDQokH4eHA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712494802583?e=1744848000&v=beta&t=HYiJoO4OI39uYECXIJlR9oPrTh77NOc7okkWwSgAE_8",
   personalWebsiteUrl: "https://sebastianthorp.com",
   contact: {
     email: "sebastian.thorp@outlook.com",
@@ -48,7 +52,7 @@ export const RESUME_DATA = {
     ],
   },
   education: [
-     {
+    {
       school: "Norwegian University of Science and Technology (NTNU)",
       degree: "Master's Degree in Work and Organizational Psychology",
       start: "2019",
@@ -114,7 +118,8 @@ export const RESUME_DATA = {
       logo: NSNLogo,
       start: "2020",
       end: "2022",
-      description: "Executed comprehensive laboratory experiments utilizing Virtual Reality and physiological measures, encompassing recruitment, data analysis, experimental design, and scientific writing",
+      description:
+        "Executed comprehensive laboratory experiments utilizing Virtual Reality and physiological measures, encompassing recruitment, data analysis, experimental design, and scientific writing",
     },
     {
       company: "2. Bataljon/Norwegian Armed Forces",
@@ -124,7 +129,8 @@ export const RESUME_DATA = {
       logo: NSNLogo,
       start: "2013",
       end: "2015",
-      description: "I managed the conscripts’, overseeing payroll, food provisioning in camps and fields, and transportation logistics. Additionally, I played a key role in the staff's planning and coordination of field exercises. My leadership responsibilities extended to guiding conscripts in both camp and field settings, where I led physical training, maintained equipment and vehicles, and lead our team during field operations.",
+      description:
+        "I managed the conscripts’, overseeing payroll, food provisioning in camps and fields, and transportation logistics. Additionally, I played a key role in the staff's planning and coordination of field exercises. My leadership responsibilities extended to guiding conscripts in both camp and field settings, where I led physical training, maintained equipment and vehicles, and lead our team during field operations.",
     },
   ],
   skills: [
@@ -141,19 +147,16 @@ export const RESUME_DATA = {
   ],
   projects: [
     {
-      title: "The Type of Task Matters: Conformity Towards Humans Versus AI in Different Task Domains",
-      techStack: ["Conformity", "Attitudes towards AI", "Human-AI interactions", "Informative influence"],
-      description:  "Sebastian Oltedal Thorp, Kjell S. Slupphaug, Lars Morten Rimol, Simon Lervik, Simen Kristoffer B. Hoel, Simone Grassini, 2025",
-      logo: ConsultlyLogo,
-      link: {
-        label: "link",
-        href: "https://www.frontiersin.org/articles/10.3389/frvir.2024.1343872/full",
-      },
-     }, 
-    {
-      title: "Comparative analysis of spatial ability in immersive and non-immersive virtual reality: the role of sense of presence, simulation sickness and cognitive load",
-      techStack: ["Spatial Computing", "Immersion", "Spatial Ability", "Simulation Sickness", "Sense of Presence", "Cogntive Load"],
-      description:  "Sebastian Oltedal Thorp, Lars Morten Rimol, Simon Lervik, Hallvard Røe Evensmoen, Simone Grassini, March 2024",
+      title:
+        "The Type of Task Matters: Conformity Towards Humans Versus AI in Different Task Domains",
+      techStack: [
+        "Conformity",
+        "Attitudes towards AI",
+        "Human-AI interactions",
+        "Informative influence",
+      ],
+      description:
+        "Sebastian Oltedal Thorp, Kjell S. Slupphaug, Lars Morten Rimol, Simon Lervik, Simen Kristoffer B. Hoel, Simone Grassini, 2025",
       logo: ConsultlyLogo,
       link: {
         label: "link",
@@ -161,8 +164,33 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "Overcoming the productivity paradox in the public sector by managing deliberate learning",
-      techStack: ["Digital Transformations", "Deliberate Learning", "Public Sector", "Manegerial Dynamic Capabilities"],
+      title:
+        "Comparative analysis of spatial ability in immersive and non-immersive virtual reality: the role of sense of presence, simulation sickness and cognitive load",
+      techStack: [
+        "Spatial Computing",
+        "Immersion",
+        "Spatial Ability",
+        "Simulation Sickness",
+        "Sense of Presence",
+        "Cogntive Load",
+      ],
+      description:
+        "Sebastian Oltedal Thorp, Lars Morten Rimol, Simon Lervik, Hallvard Røe Evensmoen, Simone Grassini, March 2024",
+      logo: ConsultlyLogo,
+      link: {
+        label: "link",
+        href: "https://www.frontiersin.org/articles/10.3389/frvir.2024.1343872/full",
+      },
+    },
+    {
+      title:
+        "Overcoming the productivity paradox in the public sector by managing deliberate learning",
+      techStack: [
+        "Digital Transformations",
+        "Deliberate Learning",
+        "Public Sector",
+        "Manegerial Dynamic Capabilities",
+      ],
       description:
         "Linn Slettum Bjerke-Busch, Sebastian Thorp, June 2023",
       logo: MonitoLogo,
@@ -172,8 +200,15 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "Association of the big five personality traits with training effectiveness, sense of presence, and cybersickness in virtual Reality",
-      techStack: ["Virtual Reality", "Personality Traits", "Training Effectivness", "Sense of Presence", "Cybersickness"],
+      title:
+        "Association of the big five personality traits with training effectiveness, sense of presence, and cybersickness in virtual Reality",
+      techStack: [
+        "Virtual Reality",
+        "Personality Traits",
+        "Training Effectivness",
+        "Sense of Presence",
+        "Cybersickness",
+      ],
       description:
         "Sebastian Oltedal Thorp, Lars Morten Rimol, Simone Grassini, November 2022",
       logo: Minimal,
@@ -183,7 +218,8 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "Temporal development of sense of presence and cybersickness during an immersive vr experience",
+      title:
+        "Temporal development of sense of presence and cybersickness during an immersive vr experience",
       techStack: ["Virtual Reality", "Sense of Presence", "Cybersickness"],
       description:
         "Sebastian Thorp, Alexander Sævild Ree, Simone Grassini, April 2022",
@@ -194,9 +230,16 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "Evaluating the effect of multi-sensory stimulations on simulator sickness and sense of presence during HMD-mediated VR experience",
-      techStack: ["Virtual Reality", "Simulation Sickness", "Sense of Presence", "Vibrations"],
-      description: "Simone Grassini, Karin Laumann, Virginia de Martin Topranin, Sebastian Thorp, December 2021",
+      title:
+        "Evaluating the effect of multi-sensory stimulations on simulator sickness and sense of presence during HMD-mediated VR experience",
+      techStack: [
+        "Virtual Reality",
+        "Simulation Sickness",
+        "Sense of Presence",
+        "Vibrations",
+      ],
+      description:
+        "Simone Grassini, Karin Laumann, Virginia de Martin Topranin, Sebastian Thorp, December 2021",
       logo: YearProgressLogo,
       link: {
         label: "getyearprogress.com",
@@ -204,12 +247,9 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "Using electrophysiological measures to evaluate the sense of presence in immersive virtual environments: An event‐related potential study",
-      techStack: [
-        "EEG",
-        "Virtual Reality",
-        "Immersion",
-      ],
+      title:
+        "Using electrophysiological measures to evaluate the sense of presence in immersive virtual environments: An event‐related potential study",
+      techStack: ["EEG", "Virtual Reality", "Immersion"],
       description:
         "Simone Grassini, Karin Laumann, Sebastian Thorp, Virginia de Martin Topranin, August 2021",
       logo: ParabolLogo,
@@ -219,12 +259,9 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "User individual characteristics and perceived usability in immersive hmd vr: A mixed method explorative study",
-      techStack: [
-        "Spatial Computing",
-        "Virtual Reality",
-        "Personality Traits",
-      ],
+      title:
+        "User individual characteristics and perceived usability in immersive hmd vr: A mixed method explorative study",
+      techStack: ["Spatial Computing", "Virtual Reality", "Personality Traits"],
       description:
         "Simone Grassini, Mina Saghafian, Sebastian Thorp, Karin Laumann, July 2021",
       logo: EvercastLogo,
